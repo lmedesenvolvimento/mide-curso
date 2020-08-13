8053af288240bb59c8af4b536c83f224393fffcd
Damis Garcia <damisgarcia@gmail.com>
2020-08-13 11:52:14 -0300

    Deployment

--- dist/_nuxt/b5e273cabf089036bd2f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{291:function(t,e,n){"use strict";n.r(e);var r=n(2),component=Object(r.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("mide-aulas-container",[e("h1",[this._v("About! Current route is: "+this._s(this.$route.path))])])}),[],!1,null,null,null);e.default=component.exports}}]);
