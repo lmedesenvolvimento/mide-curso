c863cfa5f13551ff3ed0ca325655e8cb6ce636e3
juniorce <fg.gomesjunior@gmail.com>
2020-08-29 22:08:50 -0300

    add text tematica ii

--- pages/_unidade/_aula/artigos/unidade-III-tematica-I.vue
+++ pages/_unidade/_aula/artigos/unidade-III-tematica-I.vue
@@ -41,6 +41,328 @@
           conhecermos os principais pontos do planejamento de aula.
         </div>
       </div>
+      <p>
+        Para Passos (2016, p. 372):
+      </p>
+      <p>
+        “[...] planejar é dar sentido à ação, é questionar sobre a importância
+        das práticas pedagógicas, do conteúdo ensinado, das exigências feitas
+        ao(a) aluno(a), do tipo de avaliação empregado, das atividades
+        propostas.”
+      </p>
+      <center>
+        <quotes />
+      </center>
+
+      <div class="block-bordered">
+        <div class="columns is-vcentered">
+          <div class="column">
+            <div class="dialog-box is-right">
+              Como você organiza as atividades de um componente curricular?
+            </div>
+          </div>
+          <div class="column is-3">
+            <figure>
+              <img src="~assets/images/carla-explicacao.png" />
+            </figure>
+          </div>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column is-4">
+          <img src="~assets/images/unidade-I/marcos-unidade-1.png" />
+        </div>
+
+        <div class="column fala-marcos">
+          Não existe um modelo rígido a ser seguido, mas todo plano deve
+          apresentar uma sequência lógica que ajude na orientação das
+          atividades.
+          <br />
+          Vamos apresentar aqui um modelo baseado no que é utilizado no
+          <b>Portal do Professor</b>, para indicar como podem ser organizados um
+          plano de aula com seus componentes.
+          <br />
+          <a>ACESSAR PORTAL</a>
+        </div>
+      </div>
+      <div class="block-bordered">
+        <div class="columns is-vcentered">
+          <div class="column">
+            <div class="dialog-box is-right">
+              É importante destacar que o plano de aula não deve ser um simples
+              preenchimento de formulário.
+            </div>
+          </div>
+          <div class="column is-3">
+            <figure>
+              <img src="~assets/images/carla-explicacao.png" />
+            </figure>
+          </div>
+        </div>
+      </div>
+      <div class="block-bordered">
+        <div class="columns is-vcentered">
+          <div class="column is-3">
+            <figure>
+              <img src="~assets/images/marcos-explicacao.png" />
+            </figure>
+          </div>
+          <div class="column">
+            <div class="dialog-box is-left">
+              O Planejamento deve ser uma atividade consciente que orienta
+              professores e estudantes sobre os caminhos a serem percorridos
+              durante os processos de ensino e de aprendizagem.
+            </div>
+          </div>
+        </div>
+      </div>
+      <p>
+        Vejamos, agora, os principais componentes de uma plano de aula.
+      </p>
+      <p>
+        <strong>1. ESTRUTURA CURRICULAR</strong> <br />
+        1.1 Modalidade / nível de ensino: <br />
+        1.2. Áreas do conhecimento: <br />
+        1.3 Componente curricular: <br />
+        1.4 Unidade Temática: <br />
+        1.5 Objeto do conhecimento: <br />
+      </p>
+      <p>
+        <strong>2. DADOS DA AULA</strong> <br />
+        2.1 O que o(a) aluno(a) poderá aprender com esta aula <br />
+        2.2 Duração da atividade <br />
+        2.3 Conhecimentos prévios trabalhados pelo professor com o(a) aluno(a)
+        <br />
+        2.4 Estratégias e recursos da aula <br />
+        2.5 Avaliação <br />
+      </p>
+      <p>
+        1.2 Áreas do conhecimento:
+      </p>
+      <p>
+        Segundo a BNCC as <strong>áreas do conhecimento</strong> favorecem a
+        <strong>comunicação</strong> entre os conhecimentos e saberes de
+        diferentes componentes curriculares.Sendo elas:
+      </p>
+      <p>
+        <strong>
+          Linguagens, Matemática, Ciências da Natureza, Ciências Humanas, Ensino
+          Religioso As áreas de Linguagens e Ciências Humanas abrigam mais de um
+          componente curricular. A área de Linguagem possui 04 (quatro)
+          componente curriculares, sendo eles: Língua Portuguesa, Arte, Educação
+          Física, Língua Inglesa. Já o componente de Ciências Humanas possui o
+          componente: Geografia e História.
+        </strong>
+      </p>
+      <p>
+        As áreas de Linguagens e Ciências Humanas abrigam mais de um componente
+        curricular. A área de Linguagem possui 04 (quatro) componente
+        curriculares, sendo eles: Língua Portuguesa, Arte, Educação Física,
+        Língua Inglesa. Já o componente de Ciências Humanas possui o componente:
+        Geografia e História.
+      </p>
+      <p>
+        1.3. Componente curricular:
+      </p>
+      <p>Para a BNCC os componentes curriculares são os seguintes: <br /></p>
+      <ul>
+        <li>
+          Língua Portuguesa
+        </li>
+        <li>
+          Arte
+        </li>
+        <li>
+          Educação Física
+        </li>
+        <li>
+          Língua inglesa
+        </li>
+        <li>
+          Língua Portuguesa
+        </li>
+        <li>
+          Matemática
+        </li>
+        <li>
+          Ciências
+        </li>
+        <li>
+          Geografia
+        </li>
+        <li>
+          História
+        </li>
+        <li>
+          Ensino Religioso.
+        </li>
+      </ul>
+      <p>
+        1.4 Unidade temática:
+      </p>
+      <p>
+        Para garantir o desenvolvimento das competências específicas de uma área
+        do conhecimento, cada componente curricular é organizado em unidades
+        temáticas.
+      </p>
+      <p>
+        Para nosso curso, iremos detalhar apenas as unidades temáticas de Língua
+        Portuguesa e de Matemática segundo a BNCC, tendo em vista os RED usados
+        como exemplo na Unidade 2 e no Bloco Temático 3 (propostas de planos de
+        aulas com suporte de RED).
+      </p>
+      <p>
+        Língua Portuguesa <br />
+        Especificamente, o componente curricular de Língua Portuguesa, conceitua
+        suas unidades temáticas em Práticas de linguagem, sendo divididas em:<br />
+      </p>
+      <ul>
+        <li>
+          oralidade
+        </li>
+        <li>
+          leitura/escuta
+        </li>
+        <li>
+          produção (escrita e multissemiótica)
+        </li>
+        <li>
+          análise linguística/semiótica
+        </li>
+      </ul>
+      <p>
+        Matemática <br />
+        O componente curricular de Matemática divide suas unidades temáticas
+        em:<br />
+      </p>
+      <ul>
+        <li>
+          números
+        </li>
+        <li>
+          álgebra
+        </li>
+        <li>
+          geometria
+        </li>
+        <li>
+          grandezas e medidas
+        </li>
+        <li>
+          Probabilidade e estatística
+        </li>
+      </ul>
+      <p>
+        1.5 Objeto do conhecimento: <br />
+        São entendidos como conteúdos, conceitos e processos. Segundo a BNCC,
+        respeitando as muitas possibilidades de organização do conhecimento
+        escolar, as unidades temáticas definem um arranjo dos objetos de
+        conhecimento, ao longo do Ensino Fundamental, adequado às
+        especificidades dos diferentes componentes curriculares.
+      </p>
+      <p>
+        Portanto, cada unidade temática define seus objetos de conhecimento,
+        assim como cada objeto de conhecimento se relaciona a um número variável
+        de habilidades, conforme será explicado em dados da aula.
+      </p>
+      <p>
+        2. DADOS DA AULA
+      </p>
+      <p>
+        2.1 O que o(a) aluno(a) poderá aprender com esta aula
+      </p>
+      <p>
+        Neste subitem, defina os objetivos de aprendizagem. Na BNCC, esses
+        objetivos são definidos por habilidades que expressam as aprendizagens
+        essenciais a serem asseguradas aos(as) alunos(as) nos diferentes
+        contextos escolares.
+      </p>
+      <p>
+        É preciso destacar que os critérios de organização das habilidades
+        descritos na BNCC expressam uma indicação e não devem ser tomados como
+        modelo obrigatório. A forma de apresentação adotada na BNCC “tem por
+        objetivo assegurar a clareza, a precisão e a explicitação do que se
+        espera que todos os alunos aprendam na Educação Básica, fornecendo
+        orientações para a elaboração de currículos em todo o País, adequados
+        aos diferentes contextos.” (BRASIL, 2017, p. 31).
+      </p>
+      <p>
+        2.2 Duração da atividade
+      </p>
+      <p>
+        Tempo médio necessário para que os(as) alunos(as) atinjam os objetivos
+        de aprendizagem. A duração deve ser descrita em aulas (Exs: uma aula de
+        45 minutos, 3 aulas de 45 135 minutos etc).
+      </p>
+      <p>
+        2.3 Conhecimentos prévios trabalhados pelo professor com o(a) aluno(a)
+      </p>
+      <p>
+        Em seu cotidiano, as crianças buscam compreender o significado de alguns
+        objetos ao seu redor, formando em sua estrutura cognitiva, uma rede de
+        conceitos, denominado de conhecimentos prévios.
+      </p>
+      <p>
+        Os conhecimentos prévios:
+      </p>
+      <p>
+        Indicam o que seus(uas) alunos(as) conhecem sobre os objetos de
+        conhecimentos. eles ajudam o(a) professor(a) a entender como os(as)
+        alunos(as) se relacionam com o que será ensinado. Estabelecem o que
+        os(as) alunos(as) sabem ou definem o que eles deveriam saber antes de
+        introduzir um novo conhecimento.
+      </p>
+      <p>
+        Durante as aulas, os professores podem fazer reflexões sobre o que
+        os(as) alunos(as) já conhecem sobre o tema, como também, o que cada um
+        já ouviu falar sobre ele.
+      </p>
+      <p>
+        2.4 Estratégias e recursos da aula
+      </p>
+      <p>
+        Neste subitem devemos descrever as técnicas que ajudarão os(as)
+        alunos(as) a construírem seus conhecimentos sobre um determinado tema.
+        Aqui é muito importante ser o mais descritivo possível. É nesse local
+        que descrevemos o passo a passo da aula e o como fazer. Devemos explicar
+        quais e como as atividades serão desenvolvidas, quais estratégias
+        didáticas serão utilizadas (rodas de conversa, circuito, jogo,
+        brincadeira, mapa conceitual etc), como os(as) alunos(as) serão
+        organizados (individualmente, em duplas ou grupos) e as reflexões que
+        faremos durante a(s) aula(s).
+      </p>
+      <p>
+        Os recursos indicam que materiais serão necessários para desenvolver as
+        atividades, como, por exemplo: textos impressos, jornais, revistas,
+        tangram, material dourado, Internet, áudios, vídeos, fotografias ou
+        outras imagens).
+      </p>
+      <p>
+        2.5 Avaliação
+      </p>
+      <p>
+        A avaliação deve descrever aquilo que se espera que os(as) alunos(as)
+        aprendam ao final da aula. Neste momento podemos refletir: As
+        habilidades descritas na aula, ou seja aquilo que os(as) alunos(as)
+        poderiam aprender, foram atingidas? Quais foram as dificuldades e
+        facilidades observadas? os(as) alunos(as) interagiram durante a aula?
+        Como se comportaram? As estratégias e os materiais facilitaram a
+        compreensão dos objetos de conhecimento?
+      </p>
+      <p>
+        Esses são alguns questionamentos que podem ajudá-los a compreender o
+        processo de avaliação da sua aula e da aprendizagem da turma.
+      </p>
+      <p>
+        Personagem Mulher: Estamos chegando ao final deste bloco temático,
+        esperamos que vocês tenham apreciado as características de um plano de
+        aula alinhados aos elementos da BNCC.
+      </p>
+      <p>
+        Personagens Homem: No próximo bloco, vamos mostrar duas sugestões de
+        planos de aula com suporte de RED: um de Língua Portuguesa e outro de
+        Matemática.
+      </p>
     </article>
   </mide-aulas-container>
 </template>
