f068af507e787d43eca3cd0864e4c922b24c6b48
juniorce <fg.gomesjunior@gmail.com>
2020-09-11 10:53:43 -0300

    page em breve

---
+++ pages/_unidade/_aula/artigos/em-breve.vue
@@ -0,0 +1,24 @@
+<template>
+  <mide-aulas-container>
+    <div id="em-breve">
+      <div class="mide-texto-inicial">
+        <p>
+          Em breve essa unidade estará disponível.
+        </p>
+      </div>
+    </div>
+  </mide-aulas-container>
+</template>
+
+<style lang="scss">
+#unidade-II-introducao {
+  .mide-explicacao {
+    float: left;
+  }
+  .mide-texto-inicial {
+    margin-top: 32px;
+    font-size: 14px;
+    text-indent: 50px;
+  }
+}
+</style>
